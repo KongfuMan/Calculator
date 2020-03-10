08f2388a4576de2900518fef1d4fad250755b71f
chenliang <chenliangsjtu@126.com>
2020-03-10 02:10:20 -0400

    fix bugs

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -23,12 +23,13 @@ class Main extends Component{
 
     componentDidMount(){
         axios.get('/index',{}).then(res=>{
-            this.setState({formulas: res.data.formulas});
+            console.log(res.data);
+            this.setState({formulas: res.data.formulas || []});
         });
         const endpoint = "http://localhost:" + (process.env.PORT || 5000);
         this.socket = socketIOClient(endpoint);
         this.socket.on("calculate", data => {
-            this.state.formulas.push(data);
+            this.state.formulas.push(data || "");
             this.setState({formulas: this.state.formulas});
         });
     }
