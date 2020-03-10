f0f32622a48a79081e7f4ba16d4cd3fe54cc23f5
chenliang <chenliangsjtu@126.com>
2020-03-10 01:59:18 -0400

    fix bugs

--- Server.js
+++ Server.js
@@ -15,6 +15,19 @@ app.use(cookieParser());
 
 var recent = new Map(); // store session id : most recent 10;
 
+if (process.env.NODE_ENV === 'production'){
+    //Express will serve up production assets
+    //like our main.js and main.css file
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html file
+    //if it does not recognize the route
+    const path = require('path');
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
 app.get('/index', function(req, res){
     var session = req.cookies.sessionID; // get
     if (!session) {
