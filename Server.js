6e950bd44acd290de27e2b4e882425940d57fc32
chenliang <chenliangsjtu@126.com>
2020-03-10 01:45:22 -0400

    fix bugs

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
+var port = (process.env.PORT || 5000);
 var cookieParser = require('cookie-parser');
 var session = require("express-session")({
         secret: "my-secret",
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 
-const port = process.env.PORT || 5000;
+const port = (process.env.PORT || 5000);
 module.exports = function(app) {
     app.use(proxy('/index', { target: 'http://localhost:' + port ,changeOrigin: true}));
 };
