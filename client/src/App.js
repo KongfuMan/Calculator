85e81874e70274eef49da0b723a7dfd531559506
chenliang <chenliangsjtu@126.com>
2020-03-10 01:51:41 -0400

    fix bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            <Main/>
+            <BrowserRouter>
+                <Route exact={true} path="/" component={Main}></Route>
+            </BrowserRouter>
         </div>
     );
 }
