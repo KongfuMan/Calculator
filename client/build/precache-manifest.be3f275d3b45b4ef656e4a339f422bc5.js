75861800ff7ed43a89e51738bd2f66cc9a03add7
chenliang <chenliangsjtu@126.com>
2020-03-10 02:04:22 -0400

    fix bugs

---
+++ client/build/precache-manifest.be3f275d3b45b4ef656e4a339f422bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea37569ce4e3e2e2820634852e84a5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37eae4f35fd1a7cf168c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "68dc9ff5095d32aae324",
+    "url": "/static/css/main.d8c56141.chunk.css"
+  },
+  {
+    "revision": "37eae4f35fd1a7cf168c",
+    "url": "/static/js/2.fe2024c7.chunk.js"
+  },
+  {
+    "revision": "7273350c9162f0cbb7ad3737a6a07c03",
+    "url": "/static/js/2.fe2024c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68dc9ff5095d32aae324",
+    "url": "/static/js/main.81edfd17.chunk.js"
+  },
+  {
+    "revision": "8a876b1e0350a7387bbc",
+    "url": "/static/js/runtime-main.f53b1e5a.js"
+  }
+]);
